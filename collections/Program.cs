5d32b607e3853984597ef916613ace845c88b0bd
孤独的蔬菜 <shuai19970217@163.com>
2017-05-27 09:58:54 +0800

    集合

--- collections/Program.cs
+++ collections/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace collections
 {
     class Program
     {
         static void Main(string[] args)
+
         {
+
+            ArrayList list = new ArrayList();
+            list.Add(1);
+            list.Add("狗蛋");
+            list.Add(false);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //将 ArrayList转换为数组
+            string[] str = new string[list.Count];
+            str= (string[]) list.ToArray(typeof(string));
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
